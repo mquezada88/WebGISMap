06dd0875b2107b6ed10d48786a9417d73f6edb40
Marco-PC\Marco <Qmarco88@gmail.com>
2016-12-10 13:21:27 -0500

    Added new library to manipulate GEOJSON files. Tested stability.

--- app/src/main/java/com/akexorcist/googledirection/sample/TransitDirectionActivity.java
+++ app/src/main/java/com/akexorcist/googledirection/sample/TransitDirectionActivity.java
@@ -37,6 +37,7 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
 
+
 import java.util.ArrayList;
 import java.util.List;
 
